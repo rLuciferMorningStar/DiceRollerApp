2274157494d9b75128efa553fe37af3846cec5ae
rLuciferMorningStar <r.lucifer2796@gmail.com>
2020-11-09 08:29:34 +0530

    Commit 2 - Working App

--- app/src/main/java/com/lessonone/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/lessonone/dicerollerapp/MainActivity.kt
@@ -2,10 +2,39 @@ package com.lessonone.dicerollerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.lessonone.dicerollerapp.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        val resultImage: ImageView = binding.diceImage
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        resultImage.setImageResource(drawableResource)
     }
 }
